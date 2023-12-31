d09f499a28ea86b9817d1d2fd5787f3a77cd8df9
Jonathan DeBarro <jdebarro@calpoly.edu>
2023-12-31 15:20:16 -0800

    Adding initial vals

---
+++ connect4.py
@@ -0,0 +1,35 @@
+
+
+def get_position_mask_bitmap(board, player):
+    position, mask = '', ''
+    # Start with right-most column
+    for j in range(6, -1, -1):
+        # Add 0-bits to sentinel 
+        mask += '0'
+        position += '0'
+        # Start with bottom row
+        for i in range(0, 6):
+            mask += ['0', '1'][board[i, j] != 0]
+            position += ['0', '1'][board[i, j] == player]
+    return int(position, 2), int(mask, 2)
+
+
+def connected_four(position):
+    # Horizontal check
+    m = position & (position >> 7)
+    if m & (m >> 14):
+        return True
+    # Diagonal \
+    m = position & (position >> 6)
+    if m & (m >> 12):
+        return True
+    # Diagonal /
+    m = position & (position >> 8)
+    if m & (m >> 16):
+        return True
+    # Vertical
+    m = position & (position >> 1)
+    if m & (m >> 2):
+        return True
+    # Nothing found
+    return False
